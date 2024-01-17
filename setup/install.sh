7eae20dbd2d80b1296325ed0703bbe822fa67d69
Payton Doud <35939909+Duod-Notyap@users.noreply.github.com>
2024-01-17 10:45:25 -0800

    install was weird with directories

--- setup/install.sh
+++ setup/install.sh
@@ -1,4 +1,6 @@
 mkdir -p $HOME/dotfiles
 sudo apt-get install git -y
 git clone https://github.com/Duod-Notyap/dotfiles $HOME/dotfiles
-sh -c $HOME/dotfiles/dotfiles.sh
+cd $HOME/dotfiles
+chmod +x ./dotfiles.sh
+./dotfiles.sh
