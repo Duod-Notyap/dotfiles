4917e1366b300acb416d51cac9b1ec6214c6b2fa
Payton Doud <35939909+Duod-Notyap@users.noreply.github.com>
2024-01-17 10:40:01 -0800

    Forgot to add install.sh

--- setup/install.sh
+++ setup/install.sh
@@ -1,3 +1,4 @@
-mkdir $HOME/dotfiles
+mkdir -p $HOME/dotfiles
+sudo apt-get install git -y
 git clone https://github.com/Duod-Notyap/dotfiles $HOME/dotfiles
-sh -c $HOME/dotfiles/dotfiles
+sh -c $HOME/dotfiles/dotfiles.sh
