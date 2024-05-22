5221cd14b3414bbbfbaed70b0b525b3f91a1d44c
Duod-Notyap <paytondoud@gmail.com>
2024-05-22 15:53:02 -0700

    attempt to implement gotosourcedef

--- .config/nvim/after/plugin/lsp.lua
+++ .config/nvim/after/plugin/lsp.lua
@@ -2,8 +2,53 @@ local neodev = require("neodev")
 neodev.setup()
 
 local config = require("lspconfig")
-
 local cmpCapabilities = require("cmp_nvim_lsp").default_capabilities()
+
+local extraCommands = {
+    findSourceDefinition = "textDocument/findSourceDefinition"
+}
+
+local handlers = { }
+handlers[extraCommands.findSourceDefinition] = function(args)
+    local result = args[vim.tbl_keys(args)[1]];
+    if result["error"] ~= nil then
+        print(result.error);
+        return
+    end
+
+    print(result.start.line)
+end
+
+local function jump_to_file_location(file, location)
+    vim.cmd.edit(file)
+    vim.lsp.util.jump_to_location(location);
+end
+
+local function go_to_source_implementation()
+    local r, c = unpack(vim.api.nvim_win_get_cursor(0));
+    local name = vim.api.nvim_buf_get_name(0);
+    vim.lsp.buf_request_all(
+        0,
+        "findSourceDefinition",
+        { line = r, offset = c, file = name },
+        handlers[extraCommands.findSourceDefinition])
+end
+
+vim.keymap.set("n", "<leader>gs", go_to_source_implementation);
+
+vim.api.nvim_create_autocmd('LspAttach', {
+    callback = function(args)
+        local client = vim.lsp.get_client_by_id(args.data.client_id)
+        if client ~= nil then
+            for k,v in ipairs(extraCommands) do
+                if client.supports_method(v) then
+                    client.handlers[v] = handlers[v];
+                end
+            end
+        end
+    end
+})
+
 
 vim.diagnostic.config({
     update_in_insert = true
@@ -48,6 +93,8 @@ config.lua_ls.setup({
     }
   }
 })
+
+config.somesass_ls.setup({})
 
 local htmlCapabilities = require("cmp_nvim_lsp").default_capabilities()
 htmlCapabilities.textDocument.completion.completionItem.snippetSupport = true
